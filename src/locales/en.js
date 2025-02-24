363e1dddc73311d711b610a699f35858d4acf350
PitchapaSa <pitchapa.sa@bdms.co.th>
2025-02-24 11:24:35 +0700

    chore: import locales

---
+++ src/locales/en.js
@@ -0,0 +1,82 @@
+export default {
+  agreement: {
+    agreement: "Agreement",
+    agree: "Agree",
+    disagree: "Disagree",
+  },
+  registration: {
+    register: "Register",
+  },
+  general: {
+    submit: "Submit",
+    back: "Back",
+    confirm: {
+      title: "Steps for using the Samitivej Virtual Hospital service.",
+      button: "Start",
+    },
+    preCall: {
+      message: "Browser not supported",
+      instruction: "Please copy the link below to open in the",
+      apology: "We apologize for the inconvenience.",
+      copy: "Copy",
+    },
+  },
+  telemedicine: {
+    call: "Call",
+  },
+  headConditions: {
+    headConditions: "ACCEPT TERMS",
+  },
+  headConditionsLogin: {
+    headConditionsLogin: "SAMITIVEJ VIRTUAL HOSPITAL",
+  },
+  headConditionsLoginSub: {
+    headConditionsLoginSub: "TERMS OF SERVICES",
+  },
+  wordingCheckBoxAgree: {
+    checkBoxAgree:
+      "I would like to receive health news and tips from Samitivej Hospital.",
+  },
+  wordingIcon: {
+    idCard: "Please prepare your passport.",
+    nurse: "Consult a nurse (No charge).",
+    doctor: "Consult a doctor.",
+  },
+  message: {
+    messageLine1:
+      "Please enter your date of birth as your password in DDMMYYYY format to proceed to your consultation. ",
+    messageLine2:
+      "For example, if your birthday is 22 March 1995, please enter 22031995.",
+    messageLine3: "Should you have any queries, please contact 02-378-9124",
+    passwordLabel: "Your Password",
+    termsLabel: "Terms and Conditions",
+    agreeToTermsLabel: "Agree to Terms and Conditions",
+    submit: "Confirm",
+    confirm: "Agree",
+  },
+  errorMessage: {
+    invalidMessage: "Invalid appointment code.",
+    invalidSession: "Error initiating session. Please close and try again.",
+    invalidPassword: "Your password is incorrect.",
+    password: "Please provide a password.",
+    conditions: "Agree to terms and conditions to continue.",
+  },
+  recommend: {
+    text: "To get the best experience with Samitivej Virtual Hospital, please use the latest version of a supported browser. If you're still having trouble, please contact",
+    tel: "02-022-2222",
+  },
+  resetPassword: {
+    title: "Change Password",
+    message1: "New Password",
+    message2: "Confirm Password",
+    message3: "please check password",
+    message4: "Confirm",
+  },
+  resetPin: {
+    title: "Change Pin",
+    message1: "New Pin",
+    message2: "Confirm Pin",
+    message3: "please check Pin",
+    message4: "Confirm",
+  },
+};
---
+++ src/locales/th.js
@@ -0,0 +1,78 @@
+export default {
+  agreement: {
+    agreement: "ข้อตกลง",
+    agree: "ตกลง",
+    disagree: "ไม่ตกลง",
+  },
+  registration: {
+    register: "สมัคร",
+  },
+  general: {
+    submit: "ยืนยัน",
+    back: "กลับ",
+    confirm: {
+      title: "ขั้นตอนการรับบริการ Samitivej Virtual Hospital",
+      button: "เริ่มสนทนา",
+    },
+    preCall: {
+      message: "เว็บเบราว์เซอร์ของท่านไม่รองรับ",
+      instruction: "กรุณาคัดลอกลิงค์ด้านล่าง เพื่อเปิดใน",
+      apology: "ขออภัยในความไม่สะดวก",
+      copy: "คัดลอก",
+    },
+  },
+  headConditions: {
+    headConditions: "ยอมรับข้อตกลง",
+  },
+  headConditionsLogin: {
+    headConditionsLogin: "ข้อกำหนดและเงื่อนไขของบริการ",
+  },
+  headConditionsLoginSub: {
+    headConditionsLoginSub: "Samitivej Virtual Hospital",
+  },
+  wordingCheckBoxAgree: {
+    checkBoxAgree: "ฉันตกลงที่จะรับข่าวสารสุขภาพจากโรงพยาบาลสมิติเวช",
+  },
+  wordingIcon: {
+    idCard: "กรุณาเตรียมบัตรประชาชน",
+    nurse: "พยาบาลซักถามอาการเบื้องต้น (ไม่มีค่าใช้จ่าย)",
+    doctor: "ปรึกษาแพทย์",
+  },
+  message: {
+    messageLine1:
+      "กรุณากรอกรหัสผ่าน โดยรหัสผ่านคือวันเกิดของท่านในรูปแบบ วันวันเดือนเดือนปีปีปีปี(ค.ศ.)",
+    messageLine2:
+      "เช่นหากท่านเกิดวันที่ 22 มีนาคม ค.ศ. 1995 รหัสผ่านคือ 22031995",
+    messageLine3: "หากพบปัญหาในการใช้งาน กรุณาติดต่อ 02-378-9124",
+    passwordLabel: "รหัสผ่านของท่าน",
+    termsLabel: "ดูเงื่อนไขการใช้งาน",
+    agreeToTermsLabel: "ยอมรับเงื่อนไข",
+    submit: "ยืนยัน",
+    confirm: "ยอมรับ",
+  },
+  errorMessage: {
+    invalidMessage: "หมายเลขนัดหมายไม่ถูกต้อง",
+    invalidSession: "เจอปัญหาในการเชื่อมต่อ กรุณาปิดแล้วลองใหม่อีกครั้ง",
+    invalidPassword: "รหัสผ่านไม่ถูกต้อง",
+    password: "กรุณาใส่รหัสผ่าน",
+    conditions: "ยอมรับเงื่อนไขเพื่อใช้งาน",
+  },
+  recommend: {
+    text: "เพื่อการใช้งานที่ดีที่สุด กรุณาอัพเดทบราวเซอร์ของท่านให้เป็นเวอร์ชั่นล่าสุด หากเข้าใช้งานไม่ได้ติดต่อ",
+    tel: "02-022-2222",
+  },
+  resetPassword: {
+    title: "เปลี่ยนพาสเวิร์ด",
+    message1: "พาสเวิร์ดใหม่",
+    message2: "ยืนยันพาสเวิร์ด",
+    message3: "กรุณาตรวจสอบ พาสเวิร์ดอีกครั้ง",
+    message4: "ยืนยัน",
+  },
+  resetPin: {
+    title: "เปลี่ยนรหัส",
+    message1: "รหัสใหม่",
+    message2: "ยืนยันรหัส",
+    message3: "กรุณาตรวจสอบ รหัสอีกครั้ง",
+    message4: "ยืนยัน",
+  },
+};
