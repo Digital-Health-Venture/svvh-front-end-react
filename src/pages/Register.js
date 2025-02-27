0347ec199c37332c5d412f2cc7e250cd885cc38f
PitchapaSa <pitchapa.sa@bdms.co.th>
2025-02-27 17:35:45 +0700

    feat: implement agreement modal

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,22 @@
+import React, { useState } from "react";
+
 import RegisterForm from "../components/RegisterForm";
 
+import AgreementModal from "../components/modals/AgreementModal";
+
 export default function Register() {
+  // TO BE CHANGED DUE TO THE CONDITIONAL RENDERING
+
+  const [isModalOpen, setIsModalOpen] = useState(true); // Initially, the modal is open
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false); // Close the modal when user agrees to the terms
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-y-4">
       <RegisterForm />
+      <AgreementModal open={isModalOpen} onClose={handleCloseModal} />
     </div>
   );
 }
