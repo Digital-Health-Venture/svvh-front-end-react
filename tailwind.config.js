573dd56cf4d724bc75a2e66e6ffbccaa2fff290d
PitchapaSa <pitchapa.sa@bdms.co.th>
2025-02-28 17:27:56 +0700

    style: define colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "svvh-blue": "#0057B8",
+        "svvh-red": "#D52B1E",
+        "svvh-light-yellow": "#FFEB00",
+        "svvh-yellow": "#FFD100",
+        "svvh-light-green": "#ccdfd3",
+        "svvh-green": "#12663F",
+        "svvh-light-blue": "#00AEEF",
+        "svvh-orange": "#FF5800",
+        "svvh-purple": "#7B2E8C",
+        "svvh-dark-blue": "#00205B",
+        "svvh-gray": "#6D6E70",
+        "svvh-light-gray": "#E6E7E8",
+      },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
